827ddcf49287f5a91daf335b5e13eb37095105a8
neonedekry <koraysarioglu@yahoo.com>
2018-01-27 16:27:12 +0200

    Added files save
    Please enter the commit message for your changes. Lines starting
     master
    
    deneme
    
    with 'origin/master'.
    
    test amacli

---
+++ main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#define ELEMAN_SAYISI 5
+
+using namespace std;
+
+int arr[5]{3,6,7,4,5};
+typedef struct{
+
+    int AdtTmp;
+    int HeaterTmp;
+    int SensorHigh;
+    int SensorLow;
+    int Status;
+
+}Com1ValuesStc;
+
+typedef struct{
+
+    unsigned int bit0 : 1;
+    unsigned int bit1 : 1;
+    unsigned int bit2 : 1;
+    unsigned int bit3 : 1;
+    unsigned int bit4 : 1;
+    unsigned int bit5 : 1;
+    unsigned int bit6 : 1;
+    unsigned int bit7 : 1;
+    unsigned int rsvd : 24;
+
+}SeeBitsStc;
+bool readMemory(void* pVoid)
+{
+    Com1ValuesStc* pCom1 = static_cast<Com1ValuesStc*>(pVoid);
+
+    cout<< pCom1->AdtTmp;
+
+}
+
+bool seeBits(void* pVoid)
+{
+    SeeBitsStc* pBits = static_cast<SeeBitsStc*>(pVoid);
+
+    cout<< pBits->bit0 << endl;
+    cout<< pBits->bit1 << endl;
+    cout<< pBits->bit2 << endl;
+    cout<< pBits->bit3 << endl;
+    cout<< pBits->bit4 << endl;
+    cout<< pBits->bit5 << endl;
+    cout<< pBits->bit6 << endl;
+    cout<< pBits->bit7 << endl;
+}
+class Person{
+
+public:
+    int x;
+    Person(int x){
+        this->x = x;
+    }
+    Person(){
+
+    }
+
+    string lastName;
+    string firstName;
+
+protected:
+private:
+
+
+};
+int main()
+{
+    Person person;
+    Person personn(3);
+    Person* pPerson = &personn;
+
+    pPerson->firstName = "Koray";
+    pPerson->lastName = "asd";
+
+    cout<< pPerson->firstName << endl;
+    cout<< pPerson->x << endl;
+
+}
+
+
+
+
